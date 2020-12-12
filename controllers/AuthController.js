2bdd50b933eefdbdfa67c3399c9ba327b60c632d
kpnemre <kpnemre51@gmail.com>
2020-12-12 17:49:16 +0300

    Add save user model

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,19 +1,35 @@
-exports.authRegister = (req, res) => {
+
+const User = require("../models/UserModel");
+
+exports.authRegister = async (req, res) => {
     //TODO: Register func.
     // req.body.firstName
 
+// console.log(req)
     const {firstName, lastName, email, password} = req.body;
-    console.log(
-        "Fields:",
-        firstName,
-        lastName,
-        email,
-        password
-      );
+    // expres.json middleware sayesinde 
+    // console.log(
+    //     "Fields:",
+    //     firstName,
+    //     lastName,
+    //     email,
+    //     password
+    //   );
     // todo1: validate the fields
     // todo2: check already registered
     // todo3: crpyt password
     // todo4: save the user DB
+
+    const user= new User ({
+      firstName,
+      lastName,
+      email,
+      password // crypted password
+    });
+    // user.save();
+   await user.save();
+
+    // TODO:ERROR HANDLING FOR SAVING
     res.send("Register Completed.");
   };
   
@@ -22,3 +38,11 @@ exports.authRegister = (req, res) => {
     // TODO: Login func.
     res.send("Login Completed");
   };
+
+
+//   {
+//     "firstName": "John",
+//     "lastName": "Doe",
+//     "password": "123412341234",
+//     "email": "a@a.com" 
+// }
--- models/UserModel.js
+++ models/UserModel.js
@@ -22,7 +22,7 @@ const UserSchema = new mongoose.Schema({
         default:Date.now
     },
     
-})
+});
 
 
 
